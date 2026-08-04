1e71a43f0c0b932445c6d8941b7bd28c75e4ebae
Zinedin Arnaut <Zinedinarnaut@users.noreply.github.com>
2026-08-04 12:58:19 +0800

    Vendor Sol Engine source batch 9 of 37

---
+++ Vendor/Ryubing/src/Ryujinx.Cpu/Jit/JitCpuContext.cs
@@ -0,0 +1,70 @@
+using ARMeilleure.Common;
+using ARMeilleure.Memory;
+using ARMeilleure.Translation;
+using Ryujinx.Cpu.Signal;
+
+namespace Ryujinx.Cpu.Jit
+{
+    class JitCpuContext : ICpuContext
+    {
+        private readonly ITickSource _tickSource;
+        private readonly Translator _translator;
+
+        public JitCpuContext(ITickSource tickSource, IMemoryManager memory, bool for64Bit)
+        {
+            _tickSource = tickSource;
+            
+            bool sparse = memory.Type is not MemoryManagerType.SoftwareMmu and not MemoryManagerType.SoftwarePageTable;
+            IAddressTable<ulong> functionTable = sparse ? SparseAddressTable<ulong>.CreateForArm(for64Bit) : AddressTable<ulong>.CreateForArm(for64Bit);
+            
+            _translator = new Translator(new JitMemoryAllocator(forJit: true), memory, functionTable);
+
+            if (memory.Type.IsHostMappedOrTracked)
+            {
+                NativeSignalHandler.InitializeSignalHandler();
+            }
+
+            memory.UnmapEvent += UnmapHandler;
+        }
+
+        private void UnmapHandler(ulong address, ulong size)
+        {
+            _translator.InvalidateJitCacheRegion(address, size);
+        }
+
+        /// <inheritdoc/>
+        public IExecutionContext CreateExecutionContext(ExceptionCallbacks exceptionCallbacks)
+        {
+            return new JitExecutionContext(new JitMemoryAllocator(), _tickSource, exceptionCallbacks);
+        }
+
+        /// <inheritdoc/>
+        public void Execute(IExecutionContext context, ulong address)
+        {
+            _translator.Execute(((JitExecutionContext)context).Impl, address);
+        }
+
+        /// <inheritdoc/>
+        public void InvalidateCacheRegion(ulong address, ulong size)
+        {
+            _translator.InvalidateJitCacheRegion(address, size);
+        }
+
+        /// <inheritdoc/>
+        public IDiskCacheLoadState LoadDiskCache(string titleIdText, string displayVersion, bool enabled, string cacheSelector)
+        {
+            return new JitDiskCacheLoadState(_translator.LoadDiskCache(titleIdText, displayVersion, enabled, cacheSelector));
+        }
+
+        /// <inheritdoc/>
+        public void PrepareCodeRange(ulong address, ulong size)
+        {
+            _translator.FunctionTable.SignalCodeRange(address, size);
+            _translator.PrepareCodeRange(address, size);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
